f6367dd520b8bea817b536fd064a311050b3ff0c
qingShengzzZ <377519700@qq.com>
2020-04-27 16:08:50 +0800

    commit utils

--- foodie-service/src/main/java/cn/szh/service/impl/UserServiceImpl.java
+++ foodie-service/src/main/java/cn/szh/service/impl/UserServiceImpl.java
@@ -3,12 +3,17 @@ package cn.szh.service.impl;
 import cn.szh.mapper.UsersMapper;
 import cn.szh.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public class UserServiceImpl implements UserService {
     @Autowired
     public UsersMapper userMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public boolean queryUsernameIsExist(String username) {
+
         return false;
     }
 }
