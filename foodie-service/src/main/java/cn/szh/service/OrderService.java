4d296f1e0c18e0d62210c8a93debd971e527ab58
qingShengzzZ <377519700@qq.com>
2020-05-03 14:03:36 +0800

    update OrderService

---
+++ foodie-service/src/main/java/cn/szh/service/OrderService.java
@@ -0,0 +1,32 @@
+package cn.szh.service;
+
+import cn.szh.pojo.OrderStatus;
+import cn.szh.pojo.bo.SubmitOrderBO;
+import cn.szh.pojo.vo.OrderVO;
+
+public interface OrderService {
+    /**
+     * 用于创建订单相关信息
+     * @param submitOrderBO
+     */
+    public OrderVO createOrder(SubmitOrderBO submitOrderBO);
+
+    /**
+     * 修改订单状态
+     * @param orderId
+     * @param orderStatus
+     */
+    public void updateOrderStatus(String orderId, Integer orderStatus);
+
+    /**
+     * 查询订单状态
+     * @param orderId
+     * @return
+     */
+    public OrderStatus queryOrderStatusInfo(String orderId);
+
+    /**
+     * 关闭超时未支付订单
+     */
+    public void closeOrder();
+}
